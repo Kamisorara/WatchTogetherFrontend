17ca9bc94aceb5030a78e26f24db322e98e4fa80
Kamisorara <rakamiso575@gmail.com>
2024-12-17 13:45:29 +0800

    get other user from room

--- src/api/wt/roomApi.ts
+++ src/api/wt/roomApi.ts
@@ -1,9 +1,16 @@
 import axiosInstance from "../../utils/axios";
 
+// 创建房间
 export const createRoom = () => {
-  return axiosInstance.post(`/room/create`); 
+  return axiosInstance.post(`/room/create`);
 }
 
+// 加入房间
 export const joinRoom = (roomCode: string) => {
   return axiosInstance.post(`/room/join?roomCode=${roomCode}`);
+}
+
+// 获取房间内用户
+export const getUserDetailsInRoom = (roomCode: string) => {
+  return axiosInstance.get(`/room/get-room-user?roomCode=${roomCode}`);
 }
